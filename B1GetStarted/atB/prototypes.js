875d516fa1a0af8c4b0bf924fa47659e5c121211
Georgii Galechyan <galechyan1991@gmail.com>
2024-11-10 23:13:18 +0400

    Finished book 1 and am starting book 2.

--- B1GetStarted/atB/prototypes.js
+++ B1GetStarted/atB/prototypes.js
@@ -39,17 +39,38 @@ var reel = {
   },
 }
 
-function createReels() {
-  let reels = []
-  for (i = 0; i < 9; i++) {
-    reels.push(Object.create(reel))
-  }
+// function createReels() {
+//   let reels = []
+//   for (i = 0; i < 9; i++) {
+//     reels.push(Object.create(reel))
+//   }
+//
+//   return reels
+// }
+//
+// var slotMachine = {
+//   reels: createReels(),
+//
+//   spin() {
+//     this.reels.forEach(function spinReel(reel) {
+//       reel.spin()
+//     })
+//   },
+//
+//   display() {
+//     let [a, b, c, d, e, f, g, h, i] = this.reels
+//
+//     return `
+//       ${a.display()} | ${b.display()} | ${c.display()}
+//       ${d.display()} | ${e.display()} | ${f.display()}
+//       ${g.display()} | ${h.display()} | ${i.display()}
+//       `
+//   },
+// }
 
-  return reels
-}
-
+// Вариант из книги:
 var slotMachine = {
-  reels: createReels(),
+  reels: [Object.create(reel), Object.create(reel), Object.create(reel)],
 
   spin() {
     this.reels.forEach(function spinReel(reel) {
@@ -58,13 +79,37 @@ var slotMachine = {
   },
 
   display() {
-    let [a, b, c, d, e, f, g, h, i] = this.reels
+    var lines = []
 
-    return `
-      ${a.display()} | ${b.display()} | ${c.display()}
-      ${d.display()} | ${e.display()} | ${f.display()}
-      ${g.display()} | ${h.display()} | ${i.display()}
-      `
+    // 3 цикла (т..е делаем 3 линии)
+    for (let linesPos = -1; linesPos <= 1; linesPos++) {
+      // создаём  "Линию" из 3-х спинов в каждом цикле. В конце получится
+      // 3 линии по 3 спина.
+      let line = this.reels.map(function getSlot(reel) {
+        // Создаем временный объект slot, наследующий от объекта reel
+        var slot = Object.create(reel)
+
+        // переписываем position с элементами рандомизации, используяя
+        // временный объект slot и обноыляем его внутри display()
+        slot.position = (reel.symbols.length + reel.position + linesPos) % reel.symbols.length
+
+        // возвращаем результат display(), указывая контекстом объект slot
+        // т.е. функция display будет брать позиции из объекта slot
+        return reel.display.call(slot)
+      })
+      // Добавляем элесенты в массив lines и преобразуем содержимое
+      // массива в строку, соединяя элемента массива строкой ' | '
+      /* lines.push(
+          'line[0] | line[1] | line[2]', 'line[0] | line[1] | line[2]', 'line[0] | line[1] | line[2]'
+        ) */
+      lines.push(line.join(' | '))
+    }
+    /* `
+      'line[0] | line[1] | line[2]',
+      'line[0] | line[1] | line[2]',
+      'line[0] | line[1] | line[2]'`
+    */
+    return lines.join('\n')
   },
 }
 
